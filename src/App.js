4948a696fdd6c9e41a7b054ded8bdc8d54cd4303
Alexandra <alexa.alexa0511@gmail.com>
2023-08-21 21:43:22 +0200

    implement Alert.jsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from "react";
+import Alert from "./components/Alert";
+
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+      <div className="App">
+        <Alert type="warning" text="what is love?" />;
+      </div>
   );
 }
 
