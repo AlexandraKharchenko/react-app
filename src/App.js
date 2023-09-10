6cf5c3029c689c1fbfe1766b2ba45f7dd7d8b357
Alexandra <alexa.alexa0511@gmail.com>
2023-09-10 17:03:06 +0200

    implement MarkdownEditor.jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import '@toast-ui/editor/dist/toastui-editor.css';
+import React from 'react';
+import MarkdownEditor from "./components/MarkdownEditor";
+
+
 function App() {
   return (
     <div className="App">
-     
+      <MarkdownEditor onContentChange={console.log} />
     </div>
   );
 }
